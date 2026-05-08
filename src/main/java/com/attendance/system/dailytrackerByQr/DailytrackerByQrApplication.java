bf0545a2bab0eac8f701562911e8fd403fedcc88
riteshrijal <ritesh.rijal@festnepal.com>
2026-05-08 22:41:33 +0300

    This is the starting and first commit of attendance device

---
+++ src/main/java/com/attendance/system/dailytrackerByQr/DailytrackerByQrApplication.java
@@ -0,0 +1,14 @@
+package com.attendance.system.dailytrackerByQr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailytrackerByQrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DailytrackerByQrApplication.class, args);
+		System.out.println("Sucessfully started");
+	}
+
+}
